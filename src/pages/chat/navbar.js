ffc3f5ad95409c491c7d717e2fe585e7a915d9f4
Giovanni <111045802+Giovi06@users.noreply.github.com>
2024-01-09 21:11:59 +0100

    Add files via upload

---
+++ src/pages/chat/navbar.js
@@ -0,0 +1,146 @@
+import { React, useState } from "react";
+import { Link, useNavigate } from "react-router-dom"; // Wenn du React Router verwendest
+import styles from "./styles.module.css";
+
+const Navbar = ({ setRoom, socket, username, room }) => {
+  const navigate = useNavigate();
+
+  const leaveRoom = () => {
+    const __createdtime__ = Date.now();
+    socket.emit("leave_room", { username, room, __createdtime__ });
+    // Redirect to home page
+    navigate("/", { replace: true });
+  };
+  const [newRoom, setNewRoom] = useState("");
+
+  const joinRoom = (newRoom) => {
+    console.log("Joining room...");
+    socket.emit("join_room", { username, newRoom });
+
+    navigate("/chat", { replace: true });
+  };
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navbarNav}>
+        <li className={styles.logo}>
+          <Link to="/" className={styles.navLink}>
+            <span className={(styles.linkText, styles.logoText)}>gigachat</span>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="angle-double-right"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              className="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x"
+            >
+              <g class="fa-group">
+                <path
+                  fill="currentColor"
+                  d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
+                  className={styles.faSecondary}
+                ></path>
+                <path
+                  fill="currentColor"
+                  d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
+                  className={styles.faPrimary}
+                ></path>
+              </g>
+            </svg>
+          </Link>
+          -
+        </li>
+        <li className={styles.navItem}>
+          <button
+            className={styles.navLink}
+            onClick={() => {
+              if (room !== "Chat Room 1" && newRoom !== "Chat Room 1") {
+                leaveRoom();
+                setNewRoom("Chat Room 1");
+                joinRoom(newRoom);
+              }
+            }}
+          >
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="cat"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="svg-inline--fa fa-cat fa-w-16 fa-9x"
+            >
+              <g className="fa-group">
+                <path
+                  fill="currentColor"
+                  d="M448 96h-64l-64-64v134.4a96 96 0 0 0 192 0V32zm-72 80a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm80 0a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm-165.41 16a204.07 204.07 0 0 0-34.59 2.89V272l-43.15-64.73a183.93 183.93 0 0 0-44.37 26.17L192 304l-60.94-30.47L128 272v-80a96.1 96.1 0 0 0-96-96 32 32 0 0 0 0 64 32 32 0 0 1 32 32v256a64.06 64.06 0 0 0 64 64h176a16 16 0 0 0 16-16v-16a32 32 0 0 0-32-32h-32l128-96v144a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V289.86a126.78 126.78 0 0 1-32 4.54c-61.81 0-113.52-44.05-125.41-102.4z"
+                  className={styles.faSecondary}
+                ></path>
+                <path
+                  fill="currentColor"
+                  d="M376 144a16 16 0 1 0 16 16 16 16 0 0 0-16-16zm80 0a16 16 0 1 0 16 16 16 16 0 0 0-16-16zM131.06 273.53L192 304l-23.52-70.56a192.06 192.06 0 0 0-37.42 40.09zM256 272v-77.11a198.62 198.62 0 0 0-43.15 12.38z"
+                  className={styles.faPrimary}
+                ></path>
+              </g>
+            </svg>
+            <span className={styles.linkText}>Chat Room 1</span>
+          </button>
+        </li>
+        <li className={styles.navItem}>
+          <button
+            className={styles.navLink}
+            onClick={() => {
+              if (room !== "Chat Room 2" && newRoom !== "Chat Room 2") {
+                setNewRoom("Chat Room 2");
+                joinRoom(newRoom);
+              }
+            }}
+          >
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="alien-monster"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+              className="svg-inline--fa fa-alien-monster fa-w-18 fa-9x"
+            >
+              <g className="fa-group">
+                <path
+                  fill="currentColor"
+                  d="M560,128H528a15.99954,15.99954,0,0,0-16,16v80H480V176a15.99954,15.99954,0,0,0-16-16H416V96h48a16.00079,16.00079,0,0,0,16-16V48a15.99954,15.99954,0,0,0-16-16H432a15.99954,15.99954,0,0,0-16,16V64H368a15.99954,15.99954,0,0,0-16,16v48H224V80a15.99954,15.99954,0,0,0-16-16H160V48a15.99954,15.99954,0,0,0-16-16H112A15.99954,15.99954,0,0,0,96,48V80a16.00079,16.00079,0,0,0,16,16h48v64H112a15.99954,15.99954,0,0,0-16,16v48H64V144a15.99954,15.99954,0,0,0-16-16H16A15.99954,15.99954,0,0,0,0,144V272a16.00079,16.00079,0,0,0,16,16H64v80a16.00079,16.00079,0,0,0,16,16h48v80a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V432a15.99954,15.99954,0,0,0-16-16H192V384H384v32H336a15.99954,15.99954,0,0,0-16,16v32a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V384h48a16.00079,16.00079,0,0,0,16-16V288h48a16.00079,16.00079,0,0,0,16-16V144A15.99954,15.99954,0,0,0,560,128ZM224,320H160V224h64Zm192,0H352V224h64Z"
+                  className={styles.faSecondary}
+                ></path>
+                <path
+                  fill="currentColor"
+                  d="M160,320h64V224H160Zm192-96v96h64V224Z"
+                  className={styles.faPrimary}
+                ></path>
+              </g>
+            </svg>
+            <span className={styles.linkText}>Chat Room 2</span>
+          </button>
+        </li>
+        <li className={styles.navItem}>
+          <Link to="/settings" className={styles.navLink}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <g className="fa-group">
+                <path
+                  fill="currentColor"
+                  className="fa-secondary"
+                  d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"
+                />
+              </g>
+            </svg>
+            <span className={styles.linkText}>{username}</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/settings/index.js
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+const Settings = ({ username, room, socket }) => {
+  return <div className={styles.settingsContainer}>Settings</div>;
+};
+
+export default Settings;
